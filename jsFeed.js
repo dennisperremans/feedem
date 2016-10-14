fdec298b6a0da1bf95035f54b8c28573aa9e2a5b
Dennis Perremans <dennisperremans@Denniss-MacBook-Air.local>
2016-10-14 20:46:00 +0200

    clean up + added options

--- jsFeed.js
+++ jsFeed.js
@@ -19,14 +19,26 @@ var site = {
             }
 
         }
-
         return result;
     },
 
-    loopElements: function() {
-        //define all the elements
-        var allElements = "div, p, li, a, td, span, button, h1, h2, h3, h4, h5, h6, label",
-            words       = ['Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipisicing', 'elit', 'sed', 'do', 'eiusmod', 'tempor', 'incididunt', 'ut', 'labore', 'et', 'dolore', 'magna', 'exercitation', 'ullamco'],
+    highlightElements: function(element) {
+        var hlColour = "#eeeeff";
+        $(element).css('background-color', hlColour);
+    },
+
+    feedEm: function(options) {
+        //default options
+        var settings = $.extend({
+            highlight:          false,
+            highlightColour:    '#eeeeff'
+        }, options );
+
+        //get all the elements
+        var allElements = [].slice.call(document.querySelectorAll('[data-feed]'));
+
+        //define content
+        var words       = ['Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipisicing', 'elit', 'sed', 'do', 'eiusmod', 'tempor', 'incididunt', 'ut', 'labore', 'et', 'dolore', 'magna', 'exercitation', 'ullamco'],
             sentences   = ['Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
                            'Excepteur sint occaecat cupidatat non proident.',
                            'Excepteur sint occaecat cupidatat non proident, sunt in culpa.',
@@ -40,50 +52,45 @@ var site = {
                            '<p>Officiis cupiditate laboriosam dicta alias. Ipsum vitae, autem iusto excepturi sequi incidunt ducimus similique sit tenetur quia, perspiciatis expedita ea. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni fugiat blanditiis fuga repellendus quos aliquam, nisi accusantium laudantium, eaque quis corporis nostrum cumque alias voluptatibus vel dignissimos vero minus. Alias.</p>',
                            '<p>Neque sequi sapiente tempore facilis ducimus ullam debitis corrupti perspiciatis nihil ab architecto odio.</p>'];
 
-
         //loop over all the defined elements
         $(allElements).each(function() {
-            if ($(this).attr('data-feed')) {
+            var $this = $(this),
+                $thisData = $this.data('feed'),
+                numberOf = "",
+                arrayData = $thisData.split('=');
 
-                var $thisData = $(this).data('feed'),
-                    numberOf = "",
-                    arrayData = $thisData.split('=');
+            switch(arrayData[0]) {
+                case "w":
+                    numberOf = arrayData[1];
+                    $this.html(site.getMeRandomElements(words, numberOf));
+                break;
 
-                //split attribute value in type and numberOf
-                //w = word
-                //s = sentence
-                //p = paragraph
-                //the number after the equal sign is the number of words or sentences
+                case "s":
+                    numberOf = arrayData[1];
+                    $this.html(site.getMeRandomElements(sentences, numberOf));
+                break;
 
-                switch(arrayData[0]) {
-                    case "w":
-                        numberOf = arrayData[1];
-                        $(this).html(site.getMeRandomElements(words, numberOf));
-
-                    break;
-
-                    case "s":
-                        numberOf = arrayData[1];
-                        $(this).html(site.getMeRandomElements(sentences, numberOf));
-
-                    break;
-
-                    case "p":
-                        numberOf = arrayData[1];
-                        $(this).html(site.getMeRandomElements(paragraphs, numberOf));
-
-                    break;
-                }
+                case "p":
+                    numberOf = arrayData[1];
+                    $this.html(site.getMeRandomElements(paragraphs, numberOf));
+                break;
             }
+
+            //highlight
+            if(settings.highlight)
+                $this.css('background-color', settings.highlightColour);
+            
         });
     }
-
 };
 
 $(function(){
-    site.init({
-        debug: true
-    });
+    //site.init({
+    //    debug: true
+    //});
 
-    site.loopElements()
+    site.feedEm({
+        highlight: true,
+        highlightColour: 'grey'
+    })
 });
